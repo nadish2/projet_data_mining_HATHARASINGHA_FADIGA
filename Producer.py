ec58579ab974f3cb1908427723e3ec1fb112ba64
Nadeesha HATHARASINGHA <nadeesha.hatharasingha@gmail.com>
2022-01-07 04:11:37 +0100

    Ajout des fichiers

---
+++ Producer.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  7 01:15:44 2022
+
+@author: nadee
+"""
+
+   
+#Envoie un fichier json ligne ligne par ligne dans le bus Kafka
+import json
+from kafka import KafkaProducer
+
+#On se connecte à la machine Kafka
+producer = KafkaProducer(bootstrap_servers='192.168.33.13:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+#On récupère chaque ligne du fichier json dans une liste 
+with open('data.csv', 'r') as f:
+    listusers= f.readlines()
+
+
+for i in listusers:
+    user=i.strip()
+    print(user)
+    producer.send('projet_datamining', user)
+    
+    
